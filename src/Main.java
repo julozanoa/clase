f561ae653f3807b274124860c1e4c6d7cffc4376
Estudiante <Estudiante@BE401S103C14ING.unal.loc>
2019-04-10 09:00:07 -0500

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Main {
+    public static void main(String[] args) {
+        Perro p1 = new Perro();
+        p1.nombre = "doge";
+        System.out.println(p1.nombre);
+    }
+}
---
+++ src/Perro.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Perro {
+    public String nombre;
+    public int edad;
+    public String color;
+    public double peso;
+    
+    public void comer(){
+    
+    }
+    
+    public void ladrar(){
+        
+    }
+    
+    public void correr(){
+        
+    }
+    
+    public void dormir(){
+        
+    }
+}
